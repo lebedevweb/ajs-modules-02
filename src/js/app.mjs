a0d24b77e9471c9a128a2d314b2d119d6a1ef53e
LebedevWeb <swansever@gmail.com>
2020-12-08 07:23:15 +0300

    Project work successfully

---
+++ src/js/app.mjs
@@ -0,0 +1,6 @@
+import {
+  Game, GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame,
+} from './game.mjs';
+
+const game = new Game();
+game.start();
---
+++ src/js/game.mjs
@@ -0,0 +1,19 @@
+import Character from './domain.mjs';
+
+export class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+export class GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
+
+const defaultGame = new Game();
+export default defaultGame;
